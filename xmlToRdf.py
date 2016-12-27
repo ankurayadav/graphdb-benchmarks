bcc5a5de1a6acb13eb219d8ed22a08f663ce5e8f
Ankur Yadav <ankurayadav@gmail.com>
2016-12-28 02:13:05 +0530

    separate graphs created for Tags, Users and Posts

--- xmlToRdf.py
+++ xmlToRdf.py
@@ -22,36 +22,38 @@ print "End serializing Tags.nt data file. Time taken: %s (sec)" %(end_time-start
 
 
 # Converting Users.xml to Users.nt
+g1 = Graph()
 
 # Starting to parse Users xml data from file.
 print "Started parsing Users.xml data from file."
 start_time = time()
-g.parse("data/Users.xml", format='xml')
+g1.parse("data/Users.xml", format='xml')
 end_time = time()
 print "End parsing Users.xml data file. Time taken: %s (sec)" %(end_time-start_time)
-print "Total ntriples length: %s" %(len(g))
+print "Total ntriples length: %s" %(len(g1))
 
 # Starting to serialize Users ntriples data to file.
 print "Started serializing Users.nt data to file."
 start_time = time()
-g.serialize("data/Users.nt", format='nt')
+g1.serialize("data/Users.nt", format='nt')
 end_time = time()
 print "End serializing Users.nt data file. Time taken: %s (sec)" %(end_time-start_time)
 
 
 # Converting Posts.xml to Posts.nt
+g2 = Graph()
 
 # Starting to parse Posts xml data from file.
 print "Started parsing Posts.xml data from file."
 start_time = time()
-g.parse("data/Posts.xml", format='xml')
+g2.parse("data/Posts.xml", format='xml')
 end_time = time()
 print "End parsing Posts.xml data file. Time taken: %s (sec)" %(end_time-start_time)
-print "Total ntriples length: %s" %(len(g))
+print "Total ntriples length: %s" %(len(g2))
 
 # Starting to serialize Posts ntriples data to file.
 print "Started serializing Posts.nt data to file."
 start_time = time()
-g.serialize("data/Posts.nt", format='nt')
+g2.serialize("data/Posts.nt", format='nt')
 end_time = time()
 print "End serializing Posts.nt data file. Time taken: %s (sec)" %(end_time-start_time)
